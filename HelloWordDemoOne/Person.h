2da85734beadba7611be14546669bf078139ddc6
jerryzhang <1957299143@qq.com>
2016-11-30 17:54:03 +0800

    the third committed

---
+++ HelloWordDemoOne/Person.h
@@ -0,0 +1,16 @@
+//
+//  Person.h
+//  HelloWordDemoOne
+//
+//  Created by jerryzhang on 16/11/30.
+//  Copyright © 2016年 jerryzhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, assign) NSUInteger age;
+
+@end
---
+++ HelloWordDemoOne/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  HelloWordDemoOne
+//
+//  Created by jerryzhang on 16/11/30.
+//  Copyright © 2016年 jerryzhang. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
