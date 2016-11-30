85dbd403c82011743c988ea54cc46f6f9c3b9b72
jerryzhang <1957299143@qq.com>
2016-11-30 16:59:04 +0800

    Initial Commit

---
+++ HelloWordDemoOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWordDemoOne
+//
+//  Created by jerryzhang on 16/11/30.
+//  Copyright © 2016年 jerryzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWordDemoOne/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWordDemoOne
+//
+//  Created by jerryzhang on 16/11/30.
+//  Copyright © 2016年 jerryzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
